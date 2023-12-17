4e048e8202c27d93890d2a93639e17bf192023dd
Zlei <zhanglei520sl@126.com>
2023-12-17 12:28:34 +0800

    feat:set log level trace

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,9 +1,13 @@
 use tokio::net::TcpListener;
+use tracing::Level;
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::fmt()
+        .with_max_level(Level::TRACE)
+        .with_ansi(true)
+        .init();
     tracing::info!("Listening on{:?}", listener);
     axum::serve(listener, api::routes().into_make_service())
         .await
